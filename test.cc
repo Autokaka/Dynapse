95513ca469fee0d70b4a5089317bf2de247aed90
Autokaka <qq1909698494@gmail.com>
2023-10-31 02:15:00 +0800

    feat: add more tests

--- test.cc
+++ test.cc
@@ -6,6 +6,21 @@
 // NOLINTNEXTLINE
 using namespace dynapse;
 
+class Foo {
+ public:
+  void Print() const { std::cout << "Hello, this is " << foo_ << "!" << '\n'; }
+  [[nodiscard]] std::string Get() const { return foo_; }
+
+  // NOLINTNEXTLINE
+  int aaa = 114514;
+  // NOLINTNEXTLINE
+  float bbb = 123.5;
+  bool ccc = true;
+
+ private:
+  std::string foo_ = "foo";
+};
+
 int main() {
   // clang-format off
   DYN_REFLECT_CLASS(String,
@@ -21,8 +36,47 @@ int main() {
       }),
       DYN_PROPERTY(V, "size", make_any(GetReflect().Construct(new int(0), "int"))),
     ),
-  )
+  );
+
+  DYN_REFLECT_CLASS(Foo,
+    DYN_CONSTRUCTOR([](auto) -> void* { return new Foo(); }),
+    DYN_DESTRUCTOR([](void* ptr) { delete static_cast<Foo*>(ptr); }),
+    DYN_DECL_MEMBER_PROPS(
+      DYN_PROPERTY(RW, "aaa", [](const Any& caller, const Args& args) -> Any {
+        void* aaa_ptr = new int(caller.As<Foo*>()->aaa);
+        return GetReflect().Construct(aaa_ptr, "int");
+      }, [](const Any& caller, const Args& args) -> Any {
+        std::cout << "Foo::aaa setter invoked!" << '\n';
+        caller.As<Foo*>()->aaa = args[0].To<int>();
+        return Any::Null();
+      }),
+      DYN_PROPERTY(RW, "bbb", [](const Any& caller, const Args& args) -> Any {
+        void* bbb_ptr = new float(caller.As<Foo*>()->bbb);
+        return GetReflect().Construct(bbb_ptr, "float");
+      }, [](const Any& caller, const Args& args) -> Any {
+        std::cout << "Foo::bbb setter invoked!" << '\n';
+        caller.As<Foo*>()->bbb = args[0].To<float>();
+        return Any::Null();
+      }),
+      DYN_PROPERTY(RW, "ccc", [](const Any& caller, const Args& args) -> Any {
+        void* ccc_ptr = new bool(caller.As<Foo*>()->ccc);
+        return GetReflect().Construct(ccc_ptr, "bool");
+      }, [](const Any& caller, const Args& args) -> Any {
+        std::cout << "Foo::ccc setter invoked!" << '\n';
+        caller.As<Foo*>()->ccc = args[0].To<bool>();
+        return Any::Null();
+      }),
+    ),
+    DYN_DECL_MEMBER_FUNCS(
+      DYN_FUNCTION("Print", [](const Any& caller, const Args& args) -> Any {
+        caller.As<Foo*>()->Print();
+        return Any::Null();
+      }),
+    ),
+  );
   // clang-format on
+
+  // Test `String`
 
   auto reflect = GetReflect();
   auto string = reflect.Construct("String");
@@ -37,6 +91,22 @@ int main() {
   // NOLINTNEXTLINE
   string["size"] = reflect.Construct(new int(10), "int");
   std::cout << "String.size: " << string["size"].To<int>() << '\n';
+
+  // Test `Foo`
+
+  auto foo = reflect.Construct("Foo");
+  foo["Print"]();
+  std::cout << "Foo.aaa: " << foo["aaa"].To<int>() << '\n';
+  std::cout << "typeof Foo.aaa: " << TypeOf(foo["aaa"]) << '\n';
+  // NOLINTNEXTLINE
+  foo["aaa"] = reflect.Construct(new int(1024), "int");
+  std::cout << "Foo.aaa: " << foo["aaa"].To<int>() << '\n';
+
+  std::cout << "Foo.bbb: " << foo["bbb"].To<float>() << '\n';
+  std::cout << "typeof Foo.bbb: " << TypeOf(foo["bbb"]) << '\n';
+  // NOLINTNEXTLINE
+  foo["bbb"] = reflect.Construct(new float(1024.1024), "float");
+  std::cout << "Foo.bbb: " << foo["bbb"].To<float>() << '\n';
 
   return 0;
 }
